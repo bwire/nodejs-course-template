638753ae415924b7d197fa9da029d3c45cacd157
barbedwire <e.barbedwire@gmail.com>
2020-04-02 13:49:31 +0300

    feat: user routes

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,17 @@
-const usersRepo = require('./user.memory.repository');
+const repo = require('./user.memory.repository');
 
-const getAll = () => usersRepo.getAll();
+const getAllUsers = async () => repo.getAllUsers();
+const getUserById = async id => repo.getUserById(id);
+const createUser = async (name, login, password) =>
+  repo.createUser(name, login, password);
+const updateUser = (id, name, login, password) =>
+  repo.updateUser(id, name, login, password);
+const deleteUser = id => repo.deleteUser(id);
 
-module.exports = { getAll };
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+};
