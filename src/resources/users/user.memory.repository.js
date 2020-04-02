95a9fe02c6ad1ced813db7877ff484e8f5d925d2
barbedwire <e.barbedwire@gmail.com>
2020-04-02 13:49:31 +0300

    feat: user routes

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,55 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const User = require('./user.model');
+
+const repository = () => {
+  const users = [
+    new User({ name: 'User1', login: 'User1', password: 'password1' }),
+    new User({ name: 'User2', login: 'User2', password: 'password2' }),
+    new User({ name: 'User3', login: 'User3', password: 'password3' })
+  ];
+
+  const getAllUsers = async () => {
+    return users;
+  };
+
+  const getUserById = async id => users.find(user => user.id === id);
+
+  const createUser = async (name, login, password) => {
+    if (name) {
+      const user = new User({ name, login, password });
+      users.push(user);
+      return user;
+    }
+    return undefined;
+  };
+
+  const updateUser = async (id, name, login, password) => {
+    const idx = users.findIndex(user => user.id === id);
+    if (idx !== -1) {
+      const user = users[idx];
+      user.name = name;
+      user.login = login;
+      user.password = password;
+      return user;
+    }
+    return undefined;
+  };
+
+  const deleteUser = async id => {
+    const idx = users.findIndex(user => user.id === id);
+    if (idx !== -1) {
+      users.splice(idx, 1);
+      return 204;
+    }
+    return 404;
+  };
+
+  return {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+  };
 };
 
-module.exports = { getAll };
+module.exports = repository();
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,56 @@
 const router = require('express').Router();
 const User = require('./user.model');
-const usersService = require('./user.service');
+const service = require('./user.service');
 
 router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
+  const users = await service.getAllUsers();
   res.json(users.map(User.toResponse));
+});
+
+router.route('/').post(async (req, res) => {
+  const { name, login, password } = req.body;
+  const user = await service.createUser(name, login, password);
+  if (user !== undefined) {
+    res.statusMessage = 'The user has been created';
+    res.json(User.toResponse(user));
+  } else {
+    res.statusCode = 400;
+    res.send({ error: 'Bad request' });
+  }
+});
+
+router.route('/:id').get(async (req, res) => {
+  const user = await service.getUserById(req.params.id);
+  if (user !== undefined) {
+    res.json(User.toResponse(user));
+  } else {
+    res.statusCode = 404;
+    res.send({ error: 'User not found' });
+  }
+});
+
+router.route('/:id').put(async (req, res) => {
+  const user = await service.updateUser(
+    req.params.id,
+    req.body.name,
+    req.body.login,
+    req.body.password
+  );
+  if (user !== undefined) {
+    res.json(User.toResponse(user));
+  } else {
+    res.statusCode = 400;
+    res.send({ error: 'Bad request' });
+  }
+});
+
+router.route('/:id').delete(async (req, res) => {
+  res.statusCode = await service.deleteUser(req.params.id);
+  if (res.statusCode === 204) {
+    res.json({ status: 'The user has been deleted' });
+  } else {
+    res.send({ error: 'User not found' });
+  }
 });
 
 module.exports = router;
