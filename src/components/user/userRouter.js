c81bdbd2ece8fcb4833286344351dee358fca5d4
barbedwire <e.barbedwire@gmail.com>
2020-04-04 20:25:45 +0300

    feat: replace delete user logic

--- src/components/user/userRouter.js
+++ src/components/user/userRouter.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
-const service = require('./userService');
+const userService = require('./userService');
+const taskService = require('../task/taskService');
 
 router.route('/').get(async (req, res) => {
-  const users = await service.getAllUsers();
+  const users = await userService.getAllUsers();
   res.json(users);
 });
 
 router.route('/').post(async (req, res) => {
   const { name, login, password } = req.body;
-  const user = await service.createUser({ name, login, password });
+  const user = await userService.createUser({ name, login, password });
   if (user !== undefined) {
     res.statusMessage = 'The user has been created';
     res.json(user);
@@ -20,7 +21,7 @@ router.route('/').post(async (req, res) => {
 router.route('/:id').get(async (req, res) => {
   console.log('params', req.url);
 
-  const user = await service.getUserById(req.params.id);
+  const user = await userService.getUserById(req.params.id);
   if (user !== undefined) {
     res.json(user);
   } else {
@@ -31,7 +32,7 @@ router.route('/:id').get(async (req, res) => {
 router.route('/:id').put(async (req, res) => {
   const { id } = req.params;
   const { name, login, password } = req.body;
-  const user = await service.updateUser({ id, name, login, password });
+  const user = await userService.updateUser({ id, name, login, password });
   if (user !== undefined) {
     res.statusMessage = 'The user has been updated';
     res.json(user);
@@ -41,7 +42,11 @@ router.route('/:id').put(async (req, res) => {
 });
 
 router.route('/:id').delete(async (req, res) => {
-  const idDeleted = await service.deleteUser(req.params.id);
+  const { id } = req.params;
+  let idDeleted = await userService.deleteUser(id);
+  if (idDeleted) {
+    idDeleted = await taskService.unassignUserTasks(id);
+  }
   if (idDeleted) {
     res.status(204).send('The user has been deleted');
   } else {
